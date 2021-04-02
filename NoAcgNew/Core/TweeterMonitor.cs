05b747f1863c60a8b99d18fdc8abc5ae1fb4d047
wuyu <wuyu8512@gmail.com>
2021-04-02 17:05:53 +0800

    update

---
+++ NoAcgNew/Core/TweeterMonitor.cs
@@ -0,0 +1,78 @@
+using System;
+using Microsoft.Extensions.Logging;
+using NoAcgNew.Core;
+using Wuyu.Tool.Expansion;
+
+namespace NoAcg.Monitor
+{
+    public class TweeterMonitor : Wuyu.Tool.Common.Monitor
+    {
+        private readonly TwitterApi _twitter;
+        private readonly string _userId;
+        private readonly ILogger<TweeterMonitor> _logger;
+        public string Name { get; }
+
+        private DateTime? _lastDateTime;
+
+        public event Action<TweeterMonitor, Tweet> NewTweetEvent;
+
+        public TweeterMonitor(string name, TwitterApi twitter,ILogger<TweeterMonitor> logger) : base(name)
+        {
+            _logger = logger;
+            Name = name;
+            _twitter = twitter;
+            NewTweetEvent += (sender, tweet) => logger.LogDebug("{Name}有新的推文了", name);
+            _userId = _twitter.GetUserID(name);
+            logger.LogDebug("本次监控用户ID为：{UserId}", _userId);
+        }
+
+        protected override void Handle()
+        {
+            if (_userId.IsNull()) return;
+            var list = _twitter.GetTweets(_userId);
+
+            _logger.LogDebug("本次{Name}推文数量：{Length}", Name, list.Length);
+            if (list.Length == 0)
+            {
+                return;
+            }
+
+            if (_lastDateTime == null)
+            {
+                _lastDateTime = list[0].CreatTime;
+                return;
+            }
+
+            foreach (var tweet in list)
+            {
+                if (tweet.CreatTime <= _lastDateTime) break;
+                NewTweetEvent?.Invoke(this, tweet);
+            }
+
+            _lastDateTime = list[0].CreatTime;
+        }
+
+        // private string Translate(string text)
+        // {
+        // 	StringBuilder sb = new StringBuilder(text);
+        // 	sb.Append("\n\n翻译结果：");
+        // 	string[] array = text.Split('\n');
+        // 	for (int i = 0; i < array.Length; i++)
+        // 	{
+        // 		if (i <= 1) continue;
+        // 		sb.Append("\n" + array[i].Translate());
+        // 	}
+        // 	return sb.ToString();
+        // }
+
+        public void ClearAllNewTweetEvent()
+        {
+            if (NewTweetEvent == null) return;
+            var invocationList = this.NewTweetEvent.GetInvocationList();
+            foreach (Delegate @delegate in invocationList)
+            {
+                NewTweetEvent -= @delegate as Action<TweeterMonitor, Tweet>;
+            }
+        }
+    }
+}
---
+++ NoAcgNew/Services/TweeterMonitorManage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using NoAcg.Monitor;
+using NoAcgNew.Core;
+
+namespace NoAcgNew.Services
+{
+    public class TweeterMonitorManage
+    {
+        private readonly GlobalService _globalService;
+        private readonly TwitterApi _twitterApi;
+        private readonly IServiceProvider _serviceProvider;
+
+        public TweeterMonitorManage(GlobalService globalService,TwitterApi twitterApi,IServiceProvider serviceProvider)
+        {
+            _globalService = globalService;
+            _twitterApi = twitterApi;
+            _serviceProvider = serviceProvider;
+        }
+
+        public void StartNewMonitor(string name, Action<TweeterMonitor, Tweet> action)
+        {
+            var client = new WebClient();
+            
+            var tweeterMonitor = ActivatorUtilities.CreateInstance<TweeterMonitor>(_serviceProvider, name, _twitterApi);
+            tweeterMonitor.NewTweetEvent += action;
+        }
+    }
+}
